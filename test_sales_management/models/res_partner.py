d3c591886d2fc3e4d92716a15d97ea28eec84d40
markmeasme <markangelobarnatcha@gmail.com>
2022-06-24 02:36:23 +0800

    udpate module

---
+++ test_sales_management/models/res_partner.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    credit_limit = fields.Integer(string="Credit Limit")
+    credit = fields.Integer(Stirng="Total Receivable")
+    is_over_credit = fields.Boolean(Stirng="Over Credit")
+    act_cl_exceeded = fields.Selection([('do_nothing', 'Do Nothing'),
+                                        ('warn', 'Warn'),
+                                        ('put_on_hold', 'Put on hold')],
+                                       string="Action when credit limit is exceeded")
+
+
+    @api.onchange('credit_limit', 'credit')
+    def _onchange_credit(self):
+        if self.credit_limit > self.credit:
+            self.is_over_credit = True
+        else:
+            self.is_over_credit = False
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.onchange('partner_id')
+    def partner_id_warning_message(self):
+        if not self.partner_id:
+            exit()
+        if self.partner_id.is_over_credit:
+            act_cl_exceeded = self.partner_id.act_cl_exceeded
+            if act_cl_exceeded == 'warn':
+                return {'warning': {'title': _("Warning"), 'message': 'Warning this customer exceeded his credit limit!'}}
+            if act_cl_exceeded == 'put_on_hold':
+                self.partner_id = False
+                return {
+                    'warning': {'title': _("Warning"), 'message': 'This customer is on hold!'}}
+
